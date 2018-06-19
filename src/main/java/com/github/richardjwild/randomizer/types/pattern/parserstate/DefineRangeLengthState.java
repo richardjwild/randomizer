8cc77f652d1423b486f46da79ea77ee77cf909e4
Richard Wild <richardwild76@gmail.com>
2018-06-19 16:32:32 +0100

    Refactor DefineRangeLengthState for greater clarity

--- src/main/java/com/github/richardjwild/randomizer/types/pattern/parserstate/DefineRangeLengthState.java
+++ src/main/java/com/github/richardjwild/randomizer/types/pattern/parserstate/DefineRangeLengthState.java
@@ -8,13 +8,18 @@ import java.util.List;
 
 import static com.github.richardjwild.randomizer.localization.Messages.*;
 import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
 
 public class DefineRangeLengthState extends ParserState {
 
     private final List<Character> permittedCharacters;
-    private final StringBuilder lengthBuilder = new StringBuilder();
+    private final StringBuilder lengthDefinition = new StringBuilder();
 
-    public DefineRangeLengthState(StringPatternParser parser, StringPatternBuilder builder, List<Character> permittedCharacters) {
+    public DefineRangeLengthState(
+            StringPatternParser parser,
+            StringPatternBuilder builder,
+            List<Character> permittedCharacters)
+    {
         super(parser, builder);
         this.permittedCharacters = permittedCharacters;
     }
@@ -22,42 +27,44 @@ public class DefineRangeLengthState extends ParserState {
     @Override
     public ParserState handleCharacter(char c) {
         if (c == '}') {
-            setElementLength(lengthBuilder.toString());
+            addStringPatternElement(lengthDefinition.toString());
             return new DefineLiteralPatternState(parser, builder);
         } else {
-            lengthBuilder.append(c);
+            lengthDefinition.append(c);
             return this;
         }
     }
 
     @Override
     public void patternEnded() {
-        if (lengthBuilder.length() == 0)
+        if (lengthDefinition.length() == 0)
             throw new StringPatternParserException(UNEXPECTED_PATTERN_END_WANTED_NUMBER);
         throw new StringPatternParserException(UNEXPECTED_PATTERN_END_WANTED_NUMBER_COMMA_CLOSECURLYBRACE);
     }
 
-    private void setElementLength(String elementLengthDefinition) {
-        String[] parts = elementLengthDefinition.split(",");
-        switch (parts.length) {
-            case 1:
-                if (elementLengthDefinition.endsWith(","))
-                    throw new StringPatternParserException(MISSING_VALUE_IN_LENGTH_DEFINITION);
-                builder.addFixedLengthElement(readInt(parts[0]), permittedCharacters);
-                break;
-            case 2:
-                builder.addRandomLengthElement(readInt(parts[0]), readInt(parts[1]), permittedCharacters);
-                break;
-            default:
-                throw new StringPatternParserException(TOO_MANY_FIELDS_IN_LENGTH_DEFINITION,
-                        Integer.valueOf(parts.length).toString());
+    private void addStringPatternElement(String lengthDefinition) {
+        String[] parts = lengthDefinition.split(",");
+        if (parts.length == 1) {
+            if (lengthDefinition.endsWith(","))
+                throw new StringPatternParserException(MISSING_VALUE_IN_LENGTH_DEFINITION);
+            int length = readInt(parts[0]);
+            builder.addFixedLengthElement(length, permittedCharacters);
+        } else if (parts.length == 2) {
+            int minLength = readInt(parts[0]);
+            int maxLength = readInt(parts[1]);
+            builder.addRandomLengthElement(minLength, maxLength, permittedCharacters);
+        } else {
+            throw new StringPatternParserException(
+                    TOO_MANY_FIELDS_IN_LENGTH_DEFINITION,
+                    format("%d", parts.length));
         }
     }
 
     private int readInt(String s) {
         if (s.isEmpty())
             throw new StringPatternParserException(MISSING_VALUE_IN_LENGTH_DEFINITION);
-        return tryParseInt(s);
+        else
+            return tryParseInt(s);
     }
 
     private int tryParseInt(String s) {
